ddf92cbec17dcf35a018d0ba8f04f941ab676060
freelancersifat627e <freelancersifat627@gmail.comm>
2024-11-24 19:23:18 +0600

    solve rangeError

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,8 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  const a = 10;
-  res.send(a);
+  //   const a = 10;
+  //   res.send(a);
   res.send('Hello World!');
 });
 
